90c3949b496c7a2c40e704b72234f6743d1b21ba
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2023-04-05 14:56:45 +0200

    Writing deep learning carpets

--- deep_learning/lamp/l_fren_tur.py
+++ deep_learning/lamp/l_fren_tur.py
@@ -5,7 +5,7 @@ from standard.classificator import ClassificatorClass
 from deep_learning.strings import Strings
 
 strings = Strings()
-paths = strings.paths
+paths = strings.lamp_paths
 
 cc = ClassificatorClass(0,1,paths, times = 30, verbose = 1, validation_split=0.2, epochs=10, learning_rate=4e-4)
 cc.execute_mixed([1,2])
--- deep_learning/strings.py
+++ deep_learning/strings.py
@@ -1,5 +1,15 @@
 class Strings:
     def __init__(self):
-        self.paths = ['C:\\Users\\enzop\\Desktop\\FINALDS\\lamps\\chinese\\100\\RGB',
+        self.lamp_paths = ['C:\\Users\\enzop\\Desktop\\FINALDS\\lamps\\chinese\\100\\RGB',
          'C:\\Users\\enzop\\Desktop\\FINALDS\\lamps\\french\\100\\RGB',
          'C:\\Users\\enzop\\Desktop\\FINALDS\\lamps\\turkish\\100\\RGB']
+        
+        self.carpet_paths_str = [
+            'C:\\Users\\enzop\\Desktop\\FINALDS\\carpets_stretched\\indian\\256\\RGB',
+            'C:\\Users\\enzop\\Desktop\\FINALDS\\carpets_stretched\\japanese\\256\\RGB',
+            'C:\\Users\\enzop\\Desktop\\FINALDS\\carpets_stretched\\scandinavian\\256\\RGB']
+        
+        self.carpet_paths_bla = [
+            'C:\\Users\\enzop\\Desktop\\FINALDS\\carpets_blanked\\indian\\256\\RGB',
+            'C:\\Users\\enzop\\Desktop\\FINALDS\\carpets_blanked\\japanese\\256\\RGB',
+            'C:\\Users\\enzop\\Desktop\\FINALDS\\carpets_blanked\\scandinavian\\256\\RGB']
