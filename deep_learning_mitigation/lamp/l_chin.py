1162dabf86f0a13cd49b4bcd42d1994c3c08d6e7
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2023-05-29 08:16:01 +0200

    deleting some unused folders and some unnecessery files

--- deep_learning_mitigation/lamp/l_chin.py
+++ deep_learning_mitigation/lamp/l_chin.py
@@ -7,6 +7,6 @@ from deep_learning_mitigation.strings import Strings
 strings = Strings()
 paths = strings.lamp_paths
 file_name = 'l_chin.csv'
-for i in range(11,25):
+for i in range(-1,0):
     cc = ClassificatorClass(0, 0, paths,batch_size=4, fileName=file_name, verbose = 0, validation_split=0.2, epochs=20, learning_rate=2e-5, lambda_index=i)
     cc.execute()
--- deep_learning_mitigation/lamp/l_fren.py
+++ deep_learning_mitigation/lamp/l_fren.py
@@ -7,6 +7,6 @@ from deep_learning_mitigation.strings import Strings
 strings = Strings()
 paths = strings.lamp_paths
 file_name = 'l_fren.csv'
-for i in range(25):
-    cc = ClassificatorClass(1,0,paths,batch_size=4, fileName=file_name, lambda_index=i, verbose = 1, validation_split=0.2, epochs=20, learning_rate=2e-5)
+for i in range(-1,0):
+    cc = ClassificatorClass(1,0,paths,batch_size=4, fileName=file_name, lambda_index=i, verbose = 0, validation_split=0.2, epochs=20, learning_rate=2e-5)
     cc.execute()
