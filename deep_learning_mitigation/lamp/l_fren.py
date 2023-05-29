c181ec5a35be2c0bf7728cc2a08e911e783ff55d
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2023-05-29 11:23:02 +0200

    same as previous commit, not idea why git separated them

--- deep_learning_mitigation/lamp/l_fren.py
+++ deep_learning_mitigation/lamp/l_fren.py
@@ -7,6 +7,6 @@ from deep_learning_mitigation.strings import Strings
 strings = Strings()
 paths = strings.lamp_paths
 file_name = 'l_fren.csv'
-for i in range(-1,0):
-    cc = ClassificatorClass(1,0,paths,batch_size=4, fileName=file_name, lambda_index=i, verbose = 0, validation_split=0.2, epochs=20, learning_rate=2e-5)
+for i in range(-1,25):
+    cc = ClassificatorClass(1,0,paths,batch_size=4, fileName=file_name, lambda_index=i, verbose = 0, plot=0, validation_split=0.2, epochs=40, learning_rate=6e-5, times=20)
     cc.execute()
