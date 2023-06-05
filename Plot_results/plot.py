f29e0b0059ac0e9e63eaaa2846730063e29ed027
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2023-06-05 13:11:13 +0200

    backup of data of mitigation of lamps

--- Plot_results/plot.py
+++ Plot_results/plot.py
@@ -53,6 +53,7 @@ def extract_accuracies(base, root):
         for i in range(3):
             accuracies_output = []
             for o in range(3):
+                print(fileNames[i][o])
                 acc = get_mean_accuracy(fileNames[i][o])
                 accuracies_output.append(acc)
             accuracies_culture.append(accuracies_output)
@@ -67,11 +68,13 @@ def extract_accuracies_on_culture(base, root, culture):
         accs.append(a)
     return accs
 
-def plot_acc(title, accs):
+def plot_acc(title, accs, refs): 
     fig, ax = plt.subplots()
     plt.xlabel("Lambda parameter")
     plt.ylabel("Accuracy")
-    plt.ylim([min(accs),max(accs)])
+    miny = min(accs+refs)
+    maxy = max(accs+refs)
+    plt.ylim([miny-0.5,maxy+0.5])
     plt.grid(True)
     n_points = len(accs)
     ax.set_title(title)
@@ -83,19 +86,21 @@ def plot_acc(title, accs):
     x = np.linspace(0, 100, n_points)
     plt.xticks(ticks=x, labels=ticks_label)
     ax.plot(x, accs, linewidth=2.0)
+    for ref in refs:
+        plt.axhline(y=ref, color='r', linestyle='-')
     #ax.set_xscale('log')
     plt.show()
 
-def plot_accuracies_per_culture(base, root):
+def plot_accuracies_per_culture(base, root, ref_c1, ref_c2, ref_c3, pre_title):
     chin_accs = extract_accuracies_on_culture(base, root, 0)
     #print(chin_accs)
     fren_accs = extract_accuracies_on_culture(base, root, 1)
     #print(fren_accs)
     tur_accs = extract_accuracies_on_culture(base, root, 2)
     #print(tur_accs)
-    plot_acc('Accuracy on Chinese Culture', chin_accs)
-    plot_acc('Accuracy on French Culture', fren_accs)
-    plot_acc('Accuracy on Turkish Culture', tur_accs)
+    plot_acc(f'{pre_title} Accuracy on Chinese Culture', chin_accs, ref_c1)
+    plot_acc(f'{pre_title} Accuracy on French Culture', fren_accs, ref_c2)
+    plot_acc(f'{pre_title} Accuracy on Turkish Culture', tur_accs, ref_c3)
 
 def extract_accuracies_per_test_culture(base,root, culture):
     temp_accs = extract_accuracies(base,root)
@@ -123,10 +128,23 @@ def plot_all_accuracies(base, root):
     plot_acc('Accuracy on Turkish Culture out 2', turn_on_tur[:,2])
     
 def main():
-    base = 'l_chin'
+    chin_base = 'l_chin'
+    fren_base = 'l_fren'
+    tur_base = 'l_tur'
     root = f'../deep_learning_mitigation/lamp/'
-    plot_accuracies_per_culture(base, root)
-    plot_all_accuracies(base, root)
+    chin_on_chin_ref = 87.21
+    chin_on_fren_ref = 83.79
+    chin_on_tur_ref = 75.33
+    fren_on_chin_ref = 81.04
+    fren_on_fren_ref = 89.22
+    fren_on_tur_ref = 74.60
+    tur_on_chin_ref = 78.14
+    tur_on_fren_ref = 79.33
+    tur_on_tur_ref = 89.13
+    plot_accuracies_per_culture(chin_base, root, [chin_on_chin_ref],[chin_on_fren_ref,fren_on_fren_ref],[chin_on_tur_ref, tur_on_tur_ref], 'Chinese Model' )
+    plot_accuracies_per_culture(fren_base, root, [fren_on_fren_ref,fren_on_chin_ref],[fren_on_fren_ref],[ fren_on_tur_ref,tur_on_tur_ref], 'French Model' )
+    plot_accuracies_per_culture(tur_base, root, [chin_on_chin_ref,tur_on_chin_ref],[tur_on_fren_ref,fren_on_fren_ref],[tur_on_tur_ref], 'Turkish Model' )
+    #plot_all_accuracies(base, root)
     
 
 
