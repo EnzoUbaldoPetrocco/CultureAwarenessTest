1f8d6f8b1268f731153c0337966399f4994a6d78
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2023-06-13 10:45:31 +0200

    collecting data about lamp mitigation

--- deep_learning_mitigation/lamp/l_chin.py
+++ deep_learning_mitigation/lamp/l_chin.py
@@ -7,6 +7,6 @@ from deep_learning_mitigation.strings import Strings
 strings = Strings()
 paths = strings.lamp_paths
 file_name = 'l_chin.csv'
-for i in range(-1,25):
+for i in range(3,25):
     cc = ClassificatorClass(0, 0, paths,batch_size=4, fileName=file_name, verbose = 0, plot=0, validation_split=0.2, epochs=40, learning_rate=6e-5, lambda_index=i, times=10)
     cc.execute() 
