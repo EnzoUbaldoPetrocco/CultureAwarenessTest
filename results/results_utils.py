08b13977d6286b51cc910e7e5f8368e7b59823b3
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2023-07-20 17:20:13 +0200

    saving results

--- results/results_utils.py
+++ results/results_utils.py
@@ -73,7 +73,7 @@ def retrievs_accs(path, model, ns, pu):
             print(f'Missing data for pu value = {p}')
     return pu_accs
 
-def retrieve_mean_dev_std_accs_pu(l):
+def retrieve_mean_dev_std(l):
     m = np.mean(l)
     s = 0
     for i in l:
@@ -126,10 +126,10 @@ def get_err_std_for_every_lambda(accs_pu_cult):
     lerrs = [] # errors for each lambda
     lstds = [] # stds for each lambd
     try:
-        for i in range(len(accs_pu_cult)):
+        for i in range(len(accs_pu_cult)): # for each lambda
             rrs , stds = [], []
-            for j in range(3):
-                acc, std = retrieve_mean_dev_std_accs_pu(accs_pu_cult[i][j])
+            for j in range(3): # for each culture
+                acc, std = retrieve_mean_dev_std(accs_pu_cult[i][j])
                 err = calc_ERR(acc/100)
                 rrs.append(err)
                 stds.append(std/100)
@@ -139,12 +139,34 @@ def get_err_std_for_every_lambda(accs_pu_cult):
         ...
     return lerrs, lstds
 
+def get_CIC_std_for_every_lambda(accs_pu_cult):
+    lCIC = [] # CIC for each lambda
+    lstds = [] # stds for each lambd
+    try:
+        for i in range(len(accs_pu_cult)): # for each lambda
+            accs = np.asarray(accs_pu_cult[i], dtype=object).T
+            CICs = []
+            for j in range(len(accs)): # for each sample
+                errs = []
+                for k in range(3):
+                    er = calc_ERR(accs[j][k]/100)
+                    errs.append(er)
+                c = calc_CIC(errs)
+                CICs.append(c)
+            
+            CIC, std = retrieve_mean_dev_std(CICs)
+            lCIC.append(CIC)
+            lstds.append(std)
+    except:
+        ...
+    return lCIC, lstds
 
-def get_errs_stds_for_every_lambda(accs_pu):
+def get_errs_stds_for_every_lambda(accs_pu, pu):
     clerrs = [] # errors for each lambda for each culture
     clstds = [] # stds for each lambda for each cultures
+    accs_pu = np.asarray(accs_pu, dtype=object)
     try:
-        for i in range(3):
+        for i in range(len(pu)):
             lerrs, lstds = get_err_std_for_every_lambda(accs_pu[i])
             clerrs.append(lerrs)
             clstds.append(lstds)
@@ -152,9 +174,24 @@ def get_errs_stds_for_every_lambda(accs_pu):
         ...
     return clerrs, clstds
 
+def get_CICs_stds_for_every_lambda(accs_pu, pu):
+    clCICs = [] # errors for each lambda for each culture
+    clstds = [] # stds for each lambda for each cultures
+    accs_pu = np.asarray(accs_pu, dtype=object)
+    try:
+        for i in range(len(pu)):
+            lCICs, lstds = get_CIC_std_for_every_lambda(accs_pu[i])
+            clCICs.append(lCICs)
+            clstds.append(lstds)
+    except:
+        ...
+    return clCICs, clstds
+
 def get_lamb_for_min_CIC(errs_pu):
     clerrs = [] # errors for each lambda for each culture
     clstds = [] # stds for each lambda for each cultures
+    errs_pu = np.asarray(errs_pu, dtype=object)
+    errs_pu = list(errs_pu.T)
     for i in range(3):
         lerrs, lstds = get_err_std_for_every_lambda(errs_pu[i])
         clerrs.append(lerrs)
@@ -171,33 +208,66 @@ def get_lamb_for_min_CIC(errs_pu):
     return lambda_index
 
 def get_lamb_for_min_err(errs_pu, culture):
-    errs_pu_c = errs_pu[culture]
+    errs_pu = np.asarray(errs_pu, dtype=object)
+    errs_pu = list(errs_pu.T)
+    errs_pu_c = list(errs_pu[culture])
     minimum = min(errs_pu_c)
     lambda_index = errs_pu_c.index(minimum)
     return lambda_index
 
-def print_stats(errs_pu, stds_pu, lamb, accs):
-    print(f'LAMBDA INDEX = {lamb}')
+def print_stats(errs_pu, stds_pu, lamb, CICs, stdsCIC, tau):
+    print(f'LAMBDA INDEX = {lamb} AND TAU = {tau}')
     errors = []
     for j in range(3):
-        acc, _ = retrieve_mean_dev_std_accs_pu(accs[j][lamb])
+        #acc, _ = retrieve_mean_dev_std_accs_pu(accs[j][lamb])
         #print(f'Accuracy is {acc:.1f}+-{stds_pu[j][lamb]:.1f} on Culture {j}')
-        print(f'Error is {errs_pu[j][lamb]*100:.1f}%+-{stds_pu[j][lamb]*100:.1f}% on Culture {j}')
-        errors.append(errs_pu[j][lamb])
-    CIC = calc_CIC(errors)
-    print(f'CIC is {CIC*100:.1f} on culture {j}\n\n')
+        print(f'Error is {errs_pu[lamb][j]*100:.1f}%+-{stds_pu[lamb][j]*100:.1f}% on Culture {j}')
+        errors.append(errs_pu[lamb][j])
+    #CIC = calc_CIC(errors)
+    print(f'Mean error is {np.mean(errs_pu[lamb])*100:.1f}')
+    print(f'CIC is {CICs[lamb]*100:.1f}%+-{stdsCIC[lamb]*100:.1f}% on culture {j}\n\n')
+
+def get_lamb_metric(errs_pu, tau, CICs):
+    lerrs = []
+    for i in range(len(errs_pu)): # per each lambda
+        m = np.mean(errs_pu[i]) # ERR metric
+        lerrs.append(m)
+    lambdas =  np.argwhere(lerrs < (1+tau)*min(lerrs))
+    cs = []
+    for l in lambdas: # select the CICs that are in the ball desiderd
+        cs.append(CICs[l[0]])
+    optIndex = np.argmin(cs)
+    optIndex = list(CICs).index(cs[optIndex])
+    #optIndex = np.where(CICs == cs[optIndex])
+    optValue = CICs[optIndex]
+    #print(f'lerrs are {lerrs}')
+    #print(f'minimum err is {min(lerrs)}')
+    #print(f'lambdas are {lambdas}')
+    #print(f'rrs are {cs}')
+    #print(f'optIndex is {optIndex}')
+    #print(f'optValue is {optValue}')
+    return optIndex
+    
 
 def retrieve_statistics(p, model, ns, pu):
     print(Fore.WHITE + f'MODEL IS {model}')
     accs = retrievs_accs(p, model, ns, pu)
-    clerrs, clstds = get_errs_stds_for_every_lambda(accs)
+    clerrs, clstds = get_errs_stds_for_every_lambda(accs, pu)
+    CICs, stdsCIC = get_CICs_stds_for_every_lambda(accs, pu)
     for i in range(len(pu)):
         print(f'\nREFERRING TO PU={pu[i]}')
         # I want lambda for min errs (for each culture) and min CIC and their values
         accs_pu = accs[i]
         errs_pu, stds_pu = clerrs[i], clstds[i]
+        CICs_pu, stds_CIC_pu = CICs[i], stdsCIC[i]
         lambdas = []
+        taus = [0.05, 0.1, 0.2] # 5%, 10% and 20% of the minimum error
         if len(errs_pu)>0:
+            # Calculate the minimum CIC in the first tau% of errors
+            for tau in taus:
+                lamb = get_lamb_metric(errs_pu, tau, CICs_pu)
+                print_stats(errs_pu, stds_pu, lamb, CICs_pu, stds_CIC_pu, tau)
+            '''
             for j in range(3):
                 print(f'LAMBDA FOR MINIMUM ERROR ON CULTURE {j}')
                 l = get_lamb_for_min_err(errs_pu, j)
@@ -214,18 +284,15 @@ def retrieve_statistics(p, model, ns, pu):
             except:
                 ...
             try:
-                print_stats(errs_pu, stds_pu, 12, accs_pu)
+                print_stats(errs_pu, stds_pu, 13, accs_pu)
             except:
                 ...
             try:
-                print_stats(errs_pu, stds_pu, 24, accs_pu)
+                print_stats(errs_pu, stds_pu, 25, accs_pu)
             except:
-                ...
+                ...'''
+            
 
-
-
-# TEST FUNCTIONS MITIGATION PART
-# LAMPS
 print(Fore.RED + '\n\nMITIGATION PART\n\n')
 print(Fore.BLUE + 'LAMPS\n')
 p = '../deep_learning_mitigation/lamp'
@@ -255,8 +322,8 @@ retrieve_statistics(p, model, ns, pu)
 model = 'l_tur'
 retrieve_statistics(p, model, ns, pu)
 
-print(Fore.BLUE + '\CARPETS BLANKED\n')
-p = '../deep_learning_mitigation/carpet_blanked'
+print(Fore.BLUE + '\CARPETS blank\n')
+p = '../deep_learning_mitigation/carpet_blank'
 ns = 10
 pu = ['0,05', '0,1']
 # CHIN
@@ -297,20 +364,31 @@ def retrieve_accs_standard(path, model, ns):
 def print_errors_CIC(p, model, ns):
     print(f'MODEL IS {model}')
     accs = retrieve_accs_standard(p, model, ns)
-    errs = []
-    for j in range(3):
-        l = np.multiply(accs[j], 0.01)  
-        acc, std = retrieve_mean_dev_std_accs_pu(l)
-        #print(f'Acc: {acc:.1f} on Culture {j}')
-        er = calc_ERR(acc)
-        errs.append(er)
-        print(f'Error is {er*100:.1f}%+-{std*100:.1f}% on Culture {j}')
-    if len(errs)>=3:
-        cic = calc_CIC(errs)
-        print(f'CIC for this model is {cic*100:.1f}%\n')
+    CICs = []
+    if len(accs)>0:
+        for j in range(ns): # for each sample
+            errs = []
+            for k in range(3):
+                er = calc_ERR(accs[k][j]/100)
+                errs.append(er)
+            c = calc_CIC(errs)
+            CICs.append(c)
+        CIC, CICstd = retrieve_mean_dev_std(CICs)
+        errs = []
+        for j in range(3):
+            l = np.multiply(accs[j], 0.01)  
+            acc, std = retrieve_mean_dev_std(l)
+            #print(f'Acc: {acc:.1f} on Culture {j}')
+            er = calc_ERR(acc)
+            errs.append(er)
+            print(f'Error is {er*100:.1f}%+-{std*100:.1f}% on Culture {j}')
+        if len(errs)>=3:
+            #cic = calc_CIC(errs)
+            #print(f'CIC for this model is {cic*100:.1f}%')
+            print(f'CIC for the model is {CIC*100:.1f}%+-{CICstd*100:.1f}%\n')
 
 print(Fore.BLUE + '\nLAMPS\n')
-
+'''
 # SVM
 p = '../standard'
 # pu = 0 and LIN
@@ -333,7 +411,7 @@ model = 'rbf_fren'
 print_errors_CIC(pt, model, ns)
 model = 'rbf_tur'
 print_errors_CIC(pt, model, ns)
-
+'''
 # pu = 0 and DL
 print('DL')
 pt = '../deep_learning' + '/lamp'
@@ -344,7 +422,7 @@ print_errors_CIC(pt, model, ns)
 model = 'l_tur'
 print_errors_CIC(pt, model, ns)
 
-
+'''
 print('\nPU = 0.1')
 # pu = 0.1 and LIN
 print('LSVM')
@@ -365,7 +443,7 @@ model = 'rbf_fren'
 print_errors_CIC(pt, model, ns)
 model = 'rbf_tur'
 print_errors_CIC(pt, model, ns)
-
+'''
 # pu = 0.1 and DL
 print('DL')
 pt = '../deep_learning' + '/9010/lamp/percent0,1'
@@ -375,11 +453,11 @@ model = 'l_fren'
 print_errors_CIC(pt, model, ns)
 model = 'l_tur'
 print_errors_CIC(pt, model, ns)
-
+'''
 print('\nPU = 0.05')
 # pu = 0.05 and LIN
 print('LSVM')
-pt = p + '/0,05' + '/lin'
+pt = p + '/50' + '/lin'
 model = 'lin_chin'
 print_errors_CIC(pt, model, ns)
 model = 'lin_fren'
@@ -389,14 +467,14 @@ print_errors_CIC(pt, model, ns)
 
 # pu = 0.1 and RBF
 print('GSVM')
-pt = p + '/0,05' + '/rbf'
+pt = p + '/50' + '/rbf'
 model = 'rbf_chin'
 print_errors_CIC(pt, model, ns)
 model = 'rbf_fren'
 print_errors_CIC(pt, model, ns)
 model = 'rbf_tur'
 print_errors_CIC(pt, model, ns)
-
+'''
 # pu = 0.05 and DL
 print('DL')
 pt = '../deep_learning' + '/9010/lamp/percent0,05'
@@ -436,10 +514,10 @@ model = 'l_tur'
 print_errors_CIC(pt, model, ns)
 
 
-print(Fore.BLUE + '\CARPETS BLANKED\n')
+print(Fore.BLUE + '\CARPETS blank\n')
 print(Fore.WHITE + 'DL')
 print('\nPU = 0.0')
-pt = '../deep_learning' + '/carpet_blanked'
+pt = '../deep_learning' + '/carpet_blank'
 model = 'l_chin'
 print_errors_CIC(pt, model, ns)
 model = 'l_fren'
@@ -447,7 +525,7 @@ print_errors_CIC(pt, model, ns)
 model = 'l_tur'
 print_errors_CIC(pt, model, ns)
 print('\nPU = 0.1')
-pt = '../deep_learning' + '/9010/carpet_blanked/percent0,1'
+pt = '../deep_learning' + '/9010/carpet_blank/percent0,1'
 model = 'l_chin'
 print_errors_CIC(pt, model, ns)
 model = 'l_fren'
@@ -455,7 +533,7 @@ print_errors_CIC(pt, model, ns)
 model = 'l_tur'
 print_errors_CIC(pt, model, ns)
 print('\nPU = 0.05')
-pt = '../deep_learning' + '/9010/carpet_blanked/percent0,05'
+pt = '../deep_learning' + '/9010/carpet_blank/percent0,05'
 model = 'l_chin'
 print_errors_CIC(pt, model, ns)
 model = 'l_fren'
