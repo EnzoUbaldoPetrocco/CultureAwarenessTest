4b53aeabb0bbb501f14260acd0fbe8f97d1fc9a7
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2023-09-18 08:16:53 +0200

    little modifications

--- deep_learning/model_selection/carpet_stretch/c_ind.py
+++ deep_learning/model_selection/carpet_stretch/c_ind.py
@@ -10,5 +10,5 @@ space = [0.0, 0.05, 0.1]
 folders = ['0,0', '0,05', '0,1']
 for i in range(0,len(space)):
     j = space[i]
-    cc = ClassificatorClass(0, 0, paths,batch_size=4, fileName=folders[i] + '/c_ind.csv', verbose = 1, validation_split=0.3, epochs=7, learning_rate=4e-4, percent=j, times=1)
+    cc = ClassificatorClass(0, 0, paths,batch_size=4, fileName=folders[i] + '/c_ind.csv', verbose = 0, validation_split=0.3, epochs=7, learning_rate=4e-4, percent=j, times=10)
     cc.executenineone_model_selection()
--- deep_learning/model_selection/carpet_stretch/c_jap.py
+++ deep_learning/model_selection/carpet_stretch/c_jap.py
@@ -9,5 +9,5 @@ paths = strings.lamp_paths
 space = [0.0, 0.05, 0.1]
 for i in range(0,len(space)):
     j = space[i]
-    cc = ClassificatorClass(1,0,paths,batch_size=4, fileName='c_jap.csv', verbose = 0, validation_split=0.3, epochs=7, learning_rate=4e-4, percent=j)
+    cc = ClassificatorClass(1,0,paths,batch_size=4, fileName='c_jap.csv', verbose = 0, validation_split=0.3, epochs=7, learning_rate=4e-4, percent=j, times=10)
     cc.executenineone_model_selection()
