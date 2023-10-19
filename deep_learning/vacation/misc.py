71414aa81803f09aadbec680e01a9745a518a940
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2023-10-19 12:05:51 +0200

    uploaded some results and modified a little the launch file

--- deep_learning/vacation/misc.py
+++ deep_learning/vacation/misc.py
@@ -13,14 +13,9 @@ percent = .0
 paths = strings.carpet_paths_str
 
 
-# JAPANESE
-cc = ClassificatorClass(1,0,paths,batch_size=4, fileName='c_jap_0,0str.csv', verbose = 0, times=8, validation_split=0.2, epochs=7, learning_rate=5e-4, percent=percent)
-cc.executenineone_model_selection()
-cc = None
-sleep(5)
 
 # SCANDINAVIAN
-cc = ClassificatorClass(2,0,paths,batch_size=4, fileName='c_scan_0,0str.csv', verbose = 0, times=10, validation_split=0.2, epochs=7, learning_rate=5e-4, percent=percent)
+cc = ClassificatorClass(2,0,paths,batch_size=4, fileName='c_scan_0,0str.csv', verbose = 0, times=4, validation_split=0.2, epochs=7, learning_rate=5e-4, percent=percent)
 cc.executenineone_model_selection()
 cc = None
 sleep(5)
--- deep_learning/vacation/misc_2.py
+++ deep_learning/vacation/misc_2.py
@@ -7,70 +7,7 @@ from time import sleep
 
 def main():
     # LAMPS
-    print('Lamps mitigation')
     strings = Strings()
-    paths = strings.lamp_paths
-    file_name = 'l_chin_mit.csv'
-    culture = 0
-    space = [0.05]
-    for i in range(-1, 25):
-      percent = space[0]
-      cc = ClassificatorClass(culture,
-                              0,
-                              paths,
-                              batch_size=4,
-                              fileName=file_name,
-                              verbose=0,
-                              plot=0,
-                              validation_split=0.2,
-                              epochs=30,
-                              learning_rate=6e-5,
-                              lambda_index=i,
-                              times=10,
-                              percent=percent)
-      cc.execute()
-      cc = None
-      sleep(5)
-    file_name = 'l_fren_mit.csv'
-    culture = 1
-    for i in range(-1, 25):
-      percent = space[0]
-      cc = ClassificatorClass(culture,
-                              0,
-                              paths,
-                              batch_size=4,
-                              fileName=file_name,
-                              verbose=0,
-                              plot=0,
-                              validation_split=0.2,
-                              epochs=30,
-                              learning_rate=6e-5,
-                              lambda_index=i,
-                              times=10,
-                              percent=percent)
-      cc.execute()
-      cc = None
-      sleep(5)
-    file_name = 'l_tur_mit.csv'
-    culture = 2
-    for i in range(-1, 25):
-      percent = space[0]
-      cc = ClassificatorClass(culture,
-                              0,
-                              paths,
-                              batch_size=4,
-                              fileName=file_name,
-                              verbose=0,
-                              plot=0,
-                              validation_split=0.2,
-                              epochs=30,
-                              learning_rate=6e-5,
-                              lambda_index=i,
-                              times=10,
-                              percent=percent)
-      cc.execute()
-      cc = None
-      sleep(5)
 
     # CARPET
     print('STR Carpets mitigation')
@@ -79,24 +16,6 @@ def main():
     culture = 0
     space = [0.05, 0.1]
     for i in range(-1, 25):
-      percent = space[0]
-      cc = ClassificatorClass(culture,
-                              0,
-                              paths,
-                              batch_size=4,
-                              gpu=False,
-                              fileName=file_name,
-                              verbose=0,
-                              plot=0,
-                              validation_split=0.2,
-                              epochs=30,
-                              learning_rate=6e-5,
-                              lambda_index=i,
-                              times=10,
-                              percent=percent)
-      cc.execute()
-      cc = None
-      sleep(5)
       percent = space[1]
       cc = ClassificatorClass(culture,
                               0,
@@ -107,7 +26,7 @@ def main():
                               gpu=False,
                               plot=0,
                               validation_split=0.2,
-                              epochs=30,
+                              epochs=7,
                               learning_rate=6e-5,
                               lambda_index=i,
                               times=10,
@@ -128,7 +47,7 @@ def main():
                               gpu=False,
                               plot=0,
                               validation_split=0.2,
-                              epochs=30,
+                              epochs=7,
                               learning_rate=6e-5,
                               lambda_index=i,
                               times=10,
@@ -146,7 +65,7 @@ def main():
                               gpu=False,
                               plot=0,
                               validation_split=0.2,
-                              epochs=30,
+                              epochs=7,
                               learning_rate=6e-5,
                               lambda_index=i,
                               times=10,
@@ -166,7 +85,7 @@ def main():
                               verbose=0,
                               plot=0,
                               validation_split=0.2,
-                              epochs=30,
+                              epochs=7,
                               gpu=False,
                               learning_rate=6e-5,
                               lambda_index=i,
@@ -184,7 +103,7 @@ def main():
                               verbose=0,
                               plot=0,
                               validation_split=0.2,
-                              epochs=30,
+                              epochs=7,
                               gpu=False,
                               learning_rate=6e-5,
                               lambda_index=i,
@@ -194,127 +113,6 @@ def main():
       cc = None
       sleep(5)
 
-    # CARPET
-    print('BLA Carpets mitigation')
-    paths = strings.carpet_paths_bla
-    file_name = 'c_ind_mit.csv'
-    culture = 0
-    space = [0.05, 0.1]
-    for i in range(-1, 25):
-      percent = space[0]
-      cc = ClassificatorClass(culture,
-                              0,
-                              paths,
-                              batch_size=4,
-                              gpu=False,
-                              fileName=file_name,
-                              verbose=0,
-                              plot=0,
-                              validation_split=0.2,
-                              epochs=30,
-                              learning_rate=6e-5,
-                              lambda_index=i,
-                              times=10,
-                              percent=percent)
-      cc.execute()
-      cc = None
-      sleep(5)
-      percent = space[1]
-      cc = ClassificatorClass(culture,
-                              0,
-                              paths,
-                              batch_size=4,
-                              fileName=file_name,
-                              verbose=0,
-                              gpu=False,
-                              plot=0,
-                              validation_split=0.2,
-                              epochs=30,
-                              learning_rate=6e-5,
-                              lambda_index=i,
-                              times=10,
-                              percent=percent)
-      cc.execute()
-      cc = None
-      sleep(5)
-    file_name = 'c_jap_mit.csv'
-    culture = 1
-    for i in range(-1, 25):
-      percent = space[0]
-      cc = ClassificatorClass(culture,
-                              0,
-                              paths,
-                              batch_size=4,
-                              fileName=file_name,
-                              verbose=0,
-                              gpu=False,
-                              plot=0,
-                              validation_split=0.2,
-                              epochs=30,
-                              learning_rate=6e-5,
-                              lambda_index=i,
-                              times=10,
-                              percent=percent)
-      cc.execute()
-      cc = None
-      sleep(5)
-      percent = space[1]
-      cc = ClassificatorClass(culture,
-                              0,
-                              paths,
-                              batch_size=4,
-                              fileName=file_name,
-                              verbose=0,
-                              gpu=False,
-                              plot=0,
-                              validation_split=0.2,
-                              epochs=30,
-                              learning_rate=6e-5,
-                              lambda_index=i,
-                              times=10,
-                              percent=percent)
-      cc.execute()
-      cc = None
-      sleep(5)
-    file_name = 'c_scan_mit.csv'
-    culture = 2
-    for i in range(-1, 25):
-      percent = space[0]
-      cc = ClassificatorClass(culture,
-                              0,
-                              paths,
-                              batch_size=4,
-                              fileName=file_name,
-                              verbose=0,
-                              plot=0,
-                              validation_split=0.2,
-                              epochs=30,
-                              gpu=False,
-                              learning_rate=6e-5,
-                              lambda_index=i,
-                              times=10,
-                              percent=percent)
-      cc.execute()
-      cc = None
-      sleep(5)
-      percent = space[1]
-      cc = ClassificatorClass(culture,
-                              0,
-                              paths,
-                              batch_size=4,
-                              fileName=file_name,
-                              verbose=0,
-                              plot=0,
-                              validation_split=0.2,
-                              epochs=30,
-                              gpu=False,
-                              learning_rate=6e-5,
-                              lambda_index=i,
-                              times=10,
-                              percent=percent)
-      cc.execute()
-      cc = None
-      sleep(5)
       
 
 if __name__ == "__main__":
