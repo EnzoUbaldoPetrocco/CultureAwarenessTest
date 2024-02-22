4647ae98d5d3634b37c19532539eb850dbf161d6
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2024-02-22 09:50:38 +0100

    modification for server

--- Processing/processing.py
+++ Processing/processing.py
@@ -13,6 +13,8 @@ import numpy as np
 import tensorflow as tf
 import os
 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0" 
 
 class ProcessingClass:
     def __init__(self, shallow, lamp, gpu=False) -> None:
