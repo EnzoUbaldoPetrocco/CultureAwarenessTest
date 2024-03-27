e087125c9b67bfb606dc0026f250ab898e059322
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2024-03-27 10:47:56 +0100

    data from local

--- Utils/Visualizer/visualizer.py
+++ Utils/Visualizer/visualizer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 __author__ = "Enzo Ubaldo Petrocco"
 import sys
+
 sys.path.insert(1, "../../")
 
 from matplotlib import pyplot as plt
@@ -8,14 +9,15 @@ import numpy as np
 import pandas as pd
 from Utils.Results.Results import ResAcquisitionClass
 
+
 class VisualizerClass:
     def plot_table(self, df, title):
         fig, ax = plt.subplots()
         # hide axes
         fig.patch.set_visible(False)
-        ax.axis('off')
-        ax.axis('tight')
-        tab = ax.table(cellText=df.values, colLabels=df.columns, loc='center')
+        ax.axis("off")
+        ax.axis("tight")
+        tab = ax.table(cellText=df.values, colLabels=df.columns, loc="center")
         tab.auto_set_font_size(False)
         tab.set_fontsize(9.5)
         fig.tight_layout()
@@ -26,172 +28,82 @@ class VisualizerClass:
 def main():
     visobj = VisualizerClass()
     resacqobj = ResAcquisitionClass()
-    
+
     paths = resacqobj.get_paths(basePath="../Results/")
-    for standard in range(len(paths)):
-        for lamp in range(len(paths[standard])):
-            for culture in range(len(paths[standard][lamp])):
-                for percent in range(len(paths[standard][lamp][culture])):
-                    for augment in range(
-                        len(paths[standard][lamp][culture][percent])
-                    ):
-                        for adv in range(
-                            len(paths[standard][lamp][culture][percent][augment])
-                        ):
+
+    standards = [0, 1]
+    alg = "DL"
+    lamps = [0, 1]
+    cultures = [0, 1, 2]
+    percents = [0.05, 0.1]
+    augments = [0, 1]
+    adversary = [0, 1]
+    lambda_indeces = range(-1, 13)
+    taugments = [0, 1]
+    tadversaries = [0, 1]
+    test_g_augs = [0.01, 0.05, 0.1]
+    test_eps = [0.0005, 0.001, 0.005]
+    t_cults = [0, 1, 2]
+
+    for standard in standards:
+        for lamp in lamps:
+            for culture in cultures:
+                for percent in range(len(percents)):
+                    for augment in augments:
+                        for adv in adversary:
                             if standard:
-                                for taugment in range(
-                                    len(
-                                        paths[standard][lamp][culture][percent][
-                                            augment
-                                        ][adv]
-                                    )
-                                ):
-                                    for tadversary in range(
-                                        len(
-                                            paths[standard][lamp][culture][
-                                                percent
-                                            ][augment][adv][taugment]
-                                        )
-                                    ):
-                                        for tgaug in range(
-                                            len(
-                                                paths[standard][lamp][culture][
+                                for taugment in taugments:
+                                    for tadversary in tadversaries:
+                                        for tgaug in range(len(test_g_augs)):
+                                            
+                                            for teps in range(len(test_eps)):
+
+                                                pt = paths[standard][lamp][culture][
                                                     percent
-                                                ][augment][adv][taugment][
-                                                    tadversary
+                                                ][augment][adv][taugment][tadversary][
+                                                    tgaug
+                                                ][
+                                                    teps
                                                 ]
-                                            )
-                                        ):
-                                            for teps in range(
-                                                len(
-                                                    paths[standard][lamp][
-                                                        culture
-                                                    ][percent][augment][adv][
+
+                                                pt = pt + "res.csv"
+                                                df = pd.read_csv(pt)
+                                                visobj.plot_table(
+                                                    df[df.columns[1 : len(df.columns)]],
+                                                    pt,
+                                                )
+
+                            else:
+                                for lambda_index in lambda_indeces:
+                                    for taugment in taugments:
+                                        for tadversary in tadversaries:
+                                            for tgaug in range(len(test_g_augs)):
+                                                for teps in range(len(test_eps)):
+
+                                                    pt = paths[standard][lamp][culture][
+                                                        percent
+                                                    ][augment][adv][lambda_index][
                                                         taugment
                                                     ][
                                                         tadversary
                                                     ][
                                                         tgaug
+                                                    ][
+                                                        teps
                                                     ]
-                                                )
-                                            ):
 
-                                                pt = paths[standard][lamp][
-                                                            culture
-                                                        ][percent][augment][adv][
-                                                            taugment
-                                                        ][
-                                                            tadversary
-                                                        ][
-                                                            tgaug
-                                                        ][
-                                                            teps
-                                                        ][0]
-                                                
-                                                tempst = pt.split("/")
-                                                tempst2 = ""
-                                                for i in range(
-                                                    len(tempst) - 2
-                                                ):
-                                                    tempst2 += (
-                                                        tempst[i] + "/"
-                                                    )
-                                                pt = tempst2
-                                                df = pd.read_csv(pt)
-                                                visobj.plot_table(df[df.columns[1:9]], pt)
-                                                
-                            else:
-                                for lambda_index in range(
-                                    len(
-                                        paths[standard][lamp][culture][percent][
-                                            augment
-                                        ][adv]
-                                    )
-                                ):
-                                    for taugment in range(
-                                        len(
-                                            paths[standard][lamp][culture][
-                                                percent
-                                            ][augment][adv][lambda_index]
-                                        )
-                                    ):
-                                        for tadversary in range(
-                                            len(
-                                                paths[standard][lamp][culture][
-                                                    percent
-                                                ][augment][adv][lambda_index][
-                                                    taugment
-                                                ]
-                                            )
-                                        ):
-                                            for tgaug in range(
-                                                len(
-                                                    paths[standard][lamp][
-                                                        culture
-                                                    ][percent][augment][adv][
-                                                        lambda_index
-                                                    ][
-                                                        taugment
-                                                    ][
-                                                        tadversary
-                                                    ]
-                                                )
-                                            ):
-                                                for teps in range(
-                                                    len(
-                                                        paths[standard][lamp][
-                                                            culture
-                                                        ][percent][augment][adv][
-                                                            lambda_index
-                                                        ][
-                                                            taugment
-                                                        ][
-                                                            tadversary
-                                                        ][
-                                                            tgaug
-                                                        ]
-                                                    )
-                                                ):
-                                                    
-                                                    pt = paths[standard][
-                                                                lamp
-                                                            ][culture][percent][
-                                                                augment
-                                                            ][
-                                                                adv
-                                                            ][
-                                                                lambda_index
-                                                            ][
-                                                                taugment
-                                                            ][
-                                                                tadversary
-                                                            ][
-                                                                tgaug
-                                                            ][
-                                                                teps
-                                                            ][0]
-                                                    
-                                                    tempst = pt.split("/")
-                                                    tempst2 = ""
-                                                    for i in range(
-                                                        len(tempst) - 2
-                                                    ):
-                                                        tempst2 += (
-                                                            tempst[i] + "/"
-                                                        )
-                                                    pt = tempst2 + "res.csv"
+                                                    pt = pt + "res.csv"
+                                                    print(pt)
                                                     df = pd.read_csv(pt)
-                                                    visobj.plot_table(df[df.columns[1:9]], pt)
-
-    
-        
-    
-
+                                                    visobj.plot_table(
+                                                        df[
+                                                            df.columns[
+                                                                1 : len(df.columns)
+                                                            ]
+                                                        ],
+                                                        pt,
+                                                    )
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
