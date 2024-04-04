6369d1d21f0a6be0fa4a74ef3c137448024dd20e
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2024-04-04 10:30:28 +0200

    saving results

--- Mitigated/launch_local.py
+++ Mitigated/launch_local.py
@@ -27,6 +27,80 @@ mult = 0.25
 ks = [3,2,1,0]
 cs = [2,1,0]
 
+procObj = ProcessingClass(shallow=0, lamp=1, gpu=True)
+for c in range(3):
+    for i in range(9):
+        for k in [2,1]:
+            procObj.process(
+                standard=standard,
+                type="DL",
+                verbose_param=verbose_param,
+                learning_rate=learning_rate,
+                epochs=epochs,
+                batch_size=bs,
+                lambda_index=-1,
+                culture=c,
+                percent=0.1,
+                val_split=val_split,
+                test_split=test_split,
+                n=n,
+                augment=k % 2,
+                g_rot=g_aug,
+                g_noise=g_aug,
+                g_bright=g_aug,
+                adversary=floor(k / 2),
+                eps=eps,
+                mult=mult,
+            )
+            # NoAUg
+            print(f"Testing->aug={0};adv={0}")
+            procObj.test(
+                standard=standard,
+                culture=c,
+                augment=0,
+                g_rot=g_aug,
+                g_noise=g_aug,
+                g_bright=g_aug,
+                adversary=0,
+                eps=test_eps,
+            )
+            print(f"Testing->aug={1};adv={0}")
+            for t_g_aug in test_g_augs:
+                procObj.test(
+                        standard=standard,
+                        culture=c,
+                        augment=1,
+                        g_rot=t_g_aug,
+                        g_noise=t_g_aug,
+                        g_bright=t_g_aug,
+                        adversary=0,
+                        eps=None)
+            print(f"Testing->aug={0};adv={1}")
+            for test_ep in test_eps:
+                procObj.test(
+                            standard=standard,
+                            culture=c,
+                            augment=0,
+                            g_rot=None,
+                            g_noise=None,
+                            g_bright=None,
+                            adversary=1,
+                            eps=test_ep)
+            print(f"Testing->aug={1};adv={1}")
+            for t, t_g_aug in enumerate(test_g_augs):
+                for test_ep in test_eps:  
+                    procObj.test(
+                        standard=standard,
+                        culture=c,
+                        augment=1,
+                        g_rot=t_g_aug,
+                        g_noise=t_g_aug,
+                        g_bright=t_g_aug,
+                        adversary=1,
+                        eps=test_ep)
+                        
+            procObj.partial_clear()
+
 procObj = ProcessingClass(shallow=0, lamp=lamp, gpu=True)
 with tf.device("/CPU:0"):
         for j in range(1, 13):
