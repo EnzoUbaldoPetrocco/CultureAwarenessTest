96e51fbc57e1c3056e017a318bdef0663944f180
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2024-04-22 11:08:33 -0700

    res from server

--- Mitigated/launch3.py
+++ Mitigated/launch3.py
@@ -32,11 +32,11 @@ memory_limit = 4000
 
 procObj = ProcessingClass(shallow=0, lamp=lamp, gpu=True, memory_limit=memory_limit)
 with tf.device("/CPU:0"):
-        for j in range(0, 14):
+        for j in range(1, 13):
             for percent in percents:
                 for c in range(0,3):
                     for k in range(0,4):
-                        for i in range(3):
+                        for i in range(2):
                             print(f"Training->aug={k%2};adv={floor(k/2)}")
                             procObj.process(
                                 standard=standard,
