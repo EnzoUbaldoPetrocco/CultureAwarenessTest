e33869d93958579157cf53bf0778be6641480585
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2024-06-25 11:38:59 -0700

    Changing priorities for testing

--- Mitigated/launch.py
+++ Mitigated/launch.py
@@ -19,7 +19,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 #tf.config.set_soft_device_placement(True)
 
-memory_limit = 5000
+memory_limit = 3000
 gpus = tf.config.experimental.list_physical_devices("GPU")
 if gpus:
     # Restrict TensorFlow to only allocate 2GB of memory on the first GPU
@@ -56,13 +56,13 @@ val_split = 0.2
 test_split = 0.1
 epochs = 15
 
-g_gaugs = [0.01,0.02, 0.05, 0.1, 0.2]
+g_gaugs = [0.2, 0.1, 0.01,0.02, 0.05]
 test_g_augs = [0.01, 0.05, 0.1]
 eps = 0.03
 test_eps = [0.0005, 0.001, 0.005]
 mult = 0.25
-cs = [0,1,2]
-ks = [0,1]
+cs = [2,1,0]
+ks = [1]
 
 basePath = './'
 
