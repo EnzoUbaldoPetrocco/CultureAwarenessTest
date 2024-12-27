4e02026acc9171d44919e70eaac5a78fb98053dd
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2024-12-26 16:03:16 -0800

    Saving results from server

--- Mitigated/launch4.py
+++ Mitigated/launch4.py
@@ -25,7 +25,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 # tf.config.set_soft_device_placement(True)
 
-memory_limit = 32000
+memory_limit = 38000
 gpus = tf.config.experimental.list_physical_devices("GPU")
 if gpus:
     # Restrict TensorFlow to only allocate 2GB of memory on the first GPU
@@ -60,7 +60,7 @@ val_split = 0.2
 test_split = 0.1
 epochs = 15
 class_divisions = [0,1]
-imbalances = [  0, 1]
+imbalances = [0, 1]
 diffusion = 1
 
 g_gaugs = np.logspace(-4, 0, 6)
