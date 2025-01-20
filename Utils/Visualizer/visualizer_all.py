b08c6c03583914e857bb636dba6c4c63df8b16dd
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2025-01-20 17:08:04 +0100

    Saving vis all

--- Utils/Visualizer/visualizer_all.py
+++ Utils/Visualizer/visualizer_all.py
@@ -805,7 +805,7 @@ class Res2TabClass:
         
         AUG_X = errs[1:element_percentage["g_augs"]+1]
         AUG_Y = cics[1:element_percentage["g_augs"]+1]
-        plt.plot(AUG_X, AUG_Y, color=f"{names[j]}", label="AUG")
+        plt.plot(AUG_X, AUG_Y, color=f"{names[j]}", label="DA")
         j = j + 3
 
         DIFF_X = errs[element_percentage["g_augs"]+1:element_percentage["g_augs"]+2]
@@ -828,8 +828,8 @@ class Res2TabClass:
         plt.ylabel("CIC")
         plt.title(title)
 
-        plt.xlim((0, 55))
-        plt.ylim((0, 20))
+        plt.xlim((0, 38))
+        plt.ylim((0, 17.5))
 
         # Adding legend, which helps us recognize the curve according to it's color
         plt.legend()
