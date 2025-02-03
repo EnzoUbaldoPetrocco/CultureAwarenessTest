ebbbf7ac7b66ed4f4713dc6487d3bc1999560685
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2025-02-02 16:13:24 -0800

    Saving results from server

--- Mitigated/launch.py
+++ Mitigated/launch.py
@@ -56,8 +56,8 @@ verbose_param = 1
 n = 1000
 class_divisions = [0,1]
 imbalances = [0]
-g_gaugs = np.logspace(-3, 0, 6)
-eps = np.logspace(-3, 0, 6)
+g_gaugs = np.logspace(-3, 0, 4)
+eps = np.logspace(-3, 0, 4)
 ep = 0
 cs = [2, 1, 0]
 
