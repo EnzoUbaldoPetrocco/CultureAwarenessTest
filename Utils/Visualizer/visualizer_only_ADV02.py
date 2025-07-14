7b968d27091062693ef38b879ba7ecdab1e81fa7
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2025-07-14 21:00:42 +0200

    Saving some results: PAY ATTENTION THEY MIGHT BE OLD!!!!!

--- Utils/Visualizer/visualizer_only_ADV02.py
+++ Utils/Visualizer/visualizer_only_ADV02.py
@@ -815,7 +815,7 @@ class Res2TabClass:
 
         AUG_X = errs[1 : element_percentage["g_augs"] + 1]
         AUG_Y = cics[1 : element_percentage["g_augs"] + 1]
-        plt.plot(AUG_X, AUG_Y, color=f"{names[j]}", label="DA")
+        #plt.plot(AUG_X, AUG_Y, color=f"{names[j]}", label="DA")
         j = j + 3
 
        
