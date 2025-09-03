e37faf6718da8f87addfaeaa1c50b5522825d703
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2025-09-03 09:54:34 +0200

    zoe commit

--- Model/diffusion/diffusion_standard.py
+++ Model/diffusion/diffusion_standard.py
@@ -712,7 +712,7 @@ class DiffusionStandardModel(tf.keras.Model):
         if model_selection:
             best_kid = np.inf
             for ep in [10, 30, 50]:
-                for l_r in np.logspace(-6, -3, 4):
+                for l_r in np.logspace(-5.5, -4.5, 3):
                     print(f"training with epochs = {ep}, learning rate = {l_r}")
                     self.network = tf.keras.models.load_model('diffusion_pretrained.h5')
                     self.ema_network = tf.keras.models.load_model('ema_diffusion_pretrained.h5')
