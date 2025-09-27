7652fbbddeb38710c89a84515c67d89d39ca1728
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2025-09-27 13:00:47 -0700

    Saving results from server

--- Mitigated/launch3.py
+++ Mitigated/launch3.py
@@ -24,7 +24,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 # tf.config.set_soft_device_placement(True)
 
-memory_limit = 6800
+memory_limit = 7200
 gpus = tf.config.experimental.list_physical_devices("GPU")
 if gpus:
     # Restrict TensorFlow to only allocate 2GB of memory on the first GPU
@@ -59,7 +59,7 @@ g_gaugs = np.logspace(-4, -1, 4)
 eps = np.logspace(-3, -1, 3)
 g_aug = g_gaugs[0]
 cs = [2, 1, 0]
-lamps = [ 1, 0]
+lamps = [0, 1]
 
 ep = eps[0]
 imb = 0
